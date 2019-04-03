14e07ccaaacf981ba088e73eb5ff22043071a130
clementine contat <clementine.contat@gmail.com>
2019-04-03 17:52:39 +0100

    updated time format on message

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -3,16 +3,16 @@ import React, { Component} from 'react';
 
 class Message extends Component {
   render() {
+    const time = new Date(this.props.message.created_at).toLocaleTimeString();
     return(<div className='message'>
       <div className="message-header">
         <h3>{this.props.message.author}</h3>
-        <small>{`- ${this.props.message.created_at}`}</small>
+        <small>{`- ${time}`}</small>
       </div>
       <p>{this.props.message.content}</p>
     </div>
     );
   }
-
 }
 
 
